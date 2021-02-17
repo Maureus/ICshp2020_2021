2f56751bcbf0381ba15de98ff845cabaa2387d99
Maureus <andriiandrusenko@gmail.com>
2021-02-17 10:44:50 +0100

    added all projects

---
+++ Exercise01/Alphabet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char start = 'a';
+            const int numberOfSymbols = 25;
+
+            for (int i = 0; i <= numberOfSymbols; i++)
+            {
+                Console.Write($"{start++} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Exercise01/Exercise01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string address = "Josef Novák\nJindrišská 16\n111 50, Praha 1\n";
+            Console.Write(address);
+        }
+    }
+}
