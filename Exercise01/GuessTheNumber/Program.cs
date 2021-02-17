547ee05b54565ded502e3dc7889c4c45cc3fc8b6
Maureus <andriiandrusenko@gmail.com>
2021-02-17 16:32:24 +0100

    init GuessTheNumber

--- Exercise01/GuessTheNumber/Program.cs
+++ Exercise01/GuessTheNumber/Program.cs
@@ -10,6 +10,55 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
+            var rnd = new Random();
+            const int start = 1;
+            const int end = 101;
+            int tries = 10;
+            string input = "";
+            Console.WriteLine($"Guess number between {start} and {end - 1}");
+            var number = rnd.Next(start, end);
+            while (true)
+            {
+                if (tries <= 0)
+                {
+                    Console.WriteLine("Sorry you have lost.");
+                    break;
+                }
+
+                input = Console.ReadLine();
+
+                if (input == "exit")
+                {
+                    break;
+                }
+
+                if (!int.TryParse(input, out int result))
+                {
+                    Console.WriteLine($"Somethings went wrong. Please enter number between {start} and {end - 1}.");
+                    continue;
+                }
+
+                
+
+                if (number != result)
+                {
+                    tries--;
+                    Console.Write($"You have {tries} more tries.");
+                    if (result > number)
+                    {
+                        Console.WriteLine("Try lower.");
+                    }
+                    else if (result < number)
+                    {
+                        Console.WriteLine("Try higher.");
+                    }
+                    
+                }
+                else if (number == result)
+                {
+                    Console.WriteLine($"Congratulations! You have won.");
+                }
+            }
         }
     }
 }
