d82196908e6404654ff72dfe5a6c90b7f4fe7fdb
Maureus <andriiandrusenko@gmail.com>
2021-02-17 16:39:35 +0100

    upd GuessTheNumber

--- Exercise01/GuessTheNumber/Program.cs
+++ Exercise01/GuessTheNumber/Program.cs
@@ -15,7 +15,7 @@ namespace GuessTheNumber
             const int end = 101;
             int tries = 10;
             string input = "";
-            Console.WriteLine($"Guess number between {start} and {end - 1}");
+            Console.WriteLine($"Guess the number between {start} and {end - 1}.");
             var number = rnd.Next(start, end);
             while (true)
             {
@@ -38,12 +38,20 @@ namespace GuessTheNumber
                     continue;
                 }
 
-                
+                if (result < start || result > end - 1)
+                {
+                    Console.WriteLine($"Please enter number between {start} and {end - 1}.");
+                    continue;
+                }
 
                 if (number != result)
                 {
                     tries--;
-                    Console.Write($"You have {tries} more tries.");
+                    if (tries > 0)
+                    {
+                        Console.Write($"You have {tries} more tries.");
+                    }
+
                     if (result > number)
                     {
                         Console.WriteLine("Try lower.");
@@ -52,7 +60,6 @@ namespace GuessTheNumber
                     {
                         Console.WriteLine("Try higher.");
                     }
-                    
                 }
                 else if (number == result)
                 {
