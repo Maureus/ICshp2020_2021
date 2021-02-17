85346c3cecc24828e9bd3695a56005bf856adb3d
Maureus <andriiandrusenko@gmail.com>
2021-02-17 11:55:20 +0100

    upd BirthNumber

--- Exercise01/BirthNumber/Program.cs
+++ Exercise01/BirthNumber/Program.cs
@@ -8,8 +8,31 @@ namespace BirthNumber
 {
     class Program
     {
+        // 850126/1158
+        // 01 + 50 -> female
         static void Main(string[] args)
         {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                Console.WriteLine(ParseGenderFromBirthNumber(input));
+            }
+        }
+
+        static string ParseGenderFromBirthNumber(string birthNumber)
+        {
+            if (birthNumber.Length != 11 || birthNumber[6] != '/')
+            {
+                return "Wrong birth number format.";
+            }
+
+            int genderNumber = int.Parse(birthNumber.Substring(2, 2));
+            if ((genderNumber-50) > 0 && (genderNumber - 50) <= 12)
+            {
+                return "female";
+            }
+
+            return "male";
         }
     }
 }
