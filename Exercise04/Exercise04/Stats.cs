f5f966b89c5324f7568f8f00c9bd129b97e5a9d0
Maureus <andriiandrusenko@gmail.com>
2021-03-14 21:46:44 +0100

    init solution exercise04

---
+++ Exercise04/Exercise04/Stats.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise04 {
+    public class Stats {
+        public int Correct { get; private set; }
+        public int Missed { get; private set; }
+        public double Accuracy { get; private set; }
+        public event UpdateStatsEventHandler UpdatedStats;
+
+        public Stats() {
+            Correct = 0;
+            Missed = 0;
+            Accuracy = 0;
+        }
+
+        private void OnUpdatedStats() {
+            UpdateStatsEventHandler handler = UpdatedStats;
+            if (handler != null) {
+                handler(this, new EventArgs());
+            }
+        }
+
+        public void Update(bool correctKey) {
+            if (correctKey) {
+                ++Correct;
+            }
+            else {
+                ++Missed;
+            }
+
+            if (Correct != 0) {
+                Accuracy = Math.Round( ((double) Correct / (Missed + Correct)) * 100, 2);
+            }
+            
+            OnUpdatedStats();
+        }
+
+        public delegate void UpdateStatsEventHandler(object sender, EventArgs e);
+    }
+}
